2c92781f9cef373be384c440da66a46a6bb96981
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-01 12:47:27 +0200

    start building memmove, but it's still broken

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: estettle <stettle.etan@protonmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 10:13:55 by estettle          #+#    #+#             */
-/*   Updated: 2024/10/01 11:15:49 by estettle         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:38:33 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,36 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	void	*original;
 
 	original = (void *)src;
-	// check if src and dst overlap
-	if ((size_t)(src - dst) < len || (size_t)(dst - src) < len)
+	// check if src and dst overlap and if src is BEFORE dst
+	i = dst - src;
+	if (i < 0)
+		i = -i;
+	if (i <= len)
 	{
-		; //pass
+		i = len;
+		while (i-- > 0)
+			*(char *)(dst + i) = *(char *)(src + i);
 	}
-	i = 0;
-	while (i++ > len)
-		*(char *)dst++ = *(char *)src++;
+	else
+	{
+		i = 0;
+		while (i++ < len)
+			*(char *)dst++ = *(char *)src++;
+	}
 	return (original);
+}
+
+int	main(void)
+{
+	char	*melody = "Can. You. Hear. Me?";
+	char	roxy[100] = {0};
+
+	printf("%s\n", ft_memmove(roxy, melody, 19));
+	printf("%s\n%s\n", melody, roxy);
+
+	printf("%s\n", ft_memmove(roxy+5, roxy, 19));
+	printf("%s\n%s\n", melody, roxy);
+
+	printf("%s\n", ft_memmove(roxy+10, roxy, 19));
+	printf("%s\n%s\n", melody, roxy);
 }
