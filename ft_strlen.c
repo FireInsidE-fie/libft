fc20ba0bec08d92b6c1b48d8840ec212bfddb012
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-02 10:05:18 +0200

    use size_t for strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -10,9 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(const char *s)
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
 {
-	unsigned int	count;
+	size_t	count;
 
 	count = 0;
 	while (*(s++))
