41e7f7ebe35831861cd1302e730d55b7a3c556e1
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-02 10:04:41 +0200

    fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: estettle <estettle@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 21:46:02 by estettle          #+#    #+#             */
-/*   Updated: 2024/09/28 12:27:09 by estettle         ###   ########.fr       */
+/*   Updated: 2024/10/01 23:14:49 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -26,15 +28,21 @@ int	ft_atoi(const char *str)
 		while (str[i] == '-' || str[i] == '+')
 		{
 			if (str[i] == '-')
-				sign *= -1;
+				sign = -sign;
 			i++;
 		}
 		while (str[i] >= '0' && str[i] <= '9')
 		{
-			converted += str[i];
-			converted *= 10;
-			i++;
+			converted += str[i] - '0';
+			if (str[i++ + 1])
+				converted *= 10;
 		}
 	}
 	return (converted * sign);
 }
+/*
+int	main(void)
+{
+	printf("%d\n", ft_atoi("+1234"));
+}
+*/
