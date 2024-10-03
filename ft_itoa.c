578526e183b514725128f3fb79dbad65fca09610
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-03 15:19:45 +0200

    add itoa (almost works, negatives to figure out)

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,7 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_itoa(int n)
 {
+	int		char_count;
+	int		i;
+	char	*str;
+	
+	char_count = 1;
+	i = 1;
+	if (n < 0)
+		char_count++;
+	while (n / i >= 10 || n / i <= -10)
+	{
+		char_count++;
+		i *= 10;
+	}
+	str = malloc(char_count * sizeof(char));
+	i = char_count;
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	while (i-- > 0)
+	{
+		str[i] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
 
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%s\n", ft_itoa(1234));
+	printf("%s\n", ft_itoa(-1234));
+	printf("%s\n", ft_itoa(0));
 }
