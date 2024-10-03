137f9e7f6411176db73a7c51e6811cfab929647e
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-03 16:15:00 +0200

    add putchar_fd putendl_ft and putstr_fd

--- Makefile
+++ Makefile
@@ -19,6 +19,9 @@ SRCS=ft_atoi.c \
 	 ft_memcpy.c \
 	 ft_memmove.c \
 	 ft_memset.c \
+	 ft_putchar_fd.c \
+	 ft_putendl_fd.c \
+	 ft_putstr_fd.c \
 	 ft_split.c \
 	 ft_strchr.c \
 	 ft_strdup.c \
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,7 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_putendl_fd(char *s, int fd)
 {
-
+	while (*s)
+		write(fd, s, 1);
+	write(fd, "\n", 1);
 }
