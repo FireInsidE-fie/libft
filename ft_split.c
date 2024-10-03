481119692075bb32c8550203e2f7bae3dc351c9a
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-03 14:06:45 +0200

    add draft for ft_split

--- ft_split.c
+++ ft_split.c
@@ -10,7 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	**ft_split(char const *c, char c)
-{
+#include "libft.h"
 
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	slice_count;
+	char			**slices;
+
+	i = 0;
+	slice_count = 1;
+	while (s[i++])
+	{
+		while (s[i++] && s[i] != c)
+			;
+		slice_count++;
+		while (s[i++] == c)
+			;
+	}
+	slices = malloc(slice_count * sizeof(char *));
+	if (!slices)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i++] && s[i] != c)
+			;
+		slices[j] = malloc(i * sizeof(char));
+		if (!slices)
+			return (NULL);
+		ft_strlcpy(slices[j++], s, i - 1);
+		while (s[i++] == c)
+			;
+		while (--i)
+			s++;
+	}
+	return (slices);
 }
+/*
+int	main(void)
+{
+	ft_split("Can. You. Hear. Me?", ' ');
+}
+*/
