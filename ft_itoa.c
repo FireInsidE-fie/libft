b4cc8d2ed061860f61b4508febab2946db07f951
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-03 16:43:45 +0200

    fix itoa (including edge cases)

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,36 +12,56 @@
 
 #include "libft.h"
 
+int static	ft_get_char_count(int n)
+{
+	int	i;
+	int	count;
+
+	count = 1;
+	if (n < 0)
+	{
+		count++;
+		n = -n;
+	}
+	i = 1;
+	while (n / i >= 10)
+	{
+		count++;
+		i *= 10;
+	}
+	return (count);
+}
+
 char	*ft_itoa(int n)
 {
 	int		char_count;
 	int		i;
 	char	*str;
-	
-	char_count = 1;
-	i = 1;
-	if (n < 0)
-		char_count++;
-	while (n / i >= 10 || n / i <= -10)
-	{
-		char_count++;
-		i *= 10;
-	}
+	int		is_negative;
+
+	char_count = ft_get_char_count(n);
 	str = malloc(char_count * sizeof(char));
-	i = char_count;
+	if (n == -2147483648)
+	{
+		str = "-2147483648";
+		return (str);
+	}
+	is_negative = 0;
 	if (n < 0)
 	{
 		str[0] = '-';
 		n = -n;
+		is_negative = 1;
 	}
-	while (i-- > 0)
+	i = char_count;
+	while (i-- > (0 + is_negative))
 	{
 		str[i] = (n % 10) + '0';
 		n /= 10;
 	}
 	return (str);
 }
-
+/*
 #include <stdio.h>
 
 int	main(void)
@@ -49,4 +69,7 @@ int	main(void)
 	printf("%s\n", ft_itoa(1234));
 	printf("%s\n", ft_itoa(-1234));
 	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(2147483647));
+	printf("%s\n", ft_itoa(-2147483648));
 }
+*/
