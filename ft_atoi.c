4937f3d6c7c86adfe191661c4a7fc57f596a7bf4
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-04 21:28:09 +0200

    fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 21:46:02 by estettle          #+#    #+#             */
-/*   Updated: 2024/10/01 23:14:49 by estettle         ###   ########.fr       */
+/*   Updated: 2024/10/04 20:43:20 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,19 @@ int	ft_atoi(const char *str)
 			sign = -sign;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		converted += str[i] - '0';
-		if (str[i++ + 1])
+		converted += str[i++] - '0';
+		if (str[i] >= '0' && str[i] <= '9')
 			converted *= 10;
 	}
 	return (converted * sign);
 }
 /*
+#include <stdio.h>
+
 int	main(void)
 {
-	printf("%d\n", ft_atoi("+1234"));
+	printf("%d\n", ft_atoi("+100000"));
+	printf("%d\n", ft_atoi("-123THERE IS A NYANCAT UNDER YOUR BED"));
+	printf("%d\n", atoi("-123THERE IS A NYANCAT UNDER YOUR BED"));
 }
 */
