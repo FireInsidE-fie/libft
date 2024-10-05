4aa8824f6e14e838dfc4e6c914ad09b7bee82a5c
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-05 10:09:43 +0200

    fix strlcpy, paco ok

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,12 +17,28 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	while (i < dstsize)
-	{
-		dst[i] = src[i];
-		i++;
-	}
 	if (dstsize > 0)
+	{
+		while (i < dstsize - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
+	}
 	return (ft_strlen(src));
 }
+/*
+#include <string.h>
+
+int	main(void)
+{
+	char	*melody = malloc(100);
+	char	*roxy = malloc(100);
+
+	ft_strlcpy(melody, "", 19);
+	strlcpy(roxy, "", 19);
+	ft_putstr_fd(melody, 1);
+	ft_putstr_fd(roxy, 1);
+}
+*/
