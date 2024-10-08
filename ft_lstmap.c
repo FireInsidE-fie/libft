290c600c9d7e25060feec9db9d751b0c719bb1f6
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-08 10:39:36 +0200

    fix minor norm stuff

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;	
 
 	if (!lst)
-			return (NULL);
+		return (NULL);
 	new_lst = ft_lstnew(f(lst->content));
 	if (!new_lst)
 		ft_lstclear(&new_lst, del);
