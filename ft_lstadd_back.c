60cef4b1f601d7d518b2839cd0faa81f122362e8
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-10 16:16:22 +0200

    lstadd_back passes strict testing

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: estettle <estettle@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:43:57 by estettle          #+#    #+#             */
-/*   Updated: 2024/10/07 22:09:02 by estettle         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:11:25 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	new->next = NULL;
-	if (!lst)
-		lst = &new;
-	if (!(*lst))
+	t_list	*last;
+
+	last = ft_lstlast(*lst);
+	if (!last)
 		*lst = new;
 	else
-		ft_lstlast(*lst)->next = new;
+		last->next = new;
 }
